f0655f51628915ff76cb8158e32ec4f4fcb9a248
silkka <silkka.pls@gmail.com>
2022-06-07 21:32:14 +0300

    Parse existing workspaces

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
-use std::fs;
+use std::path::Path;
+use std::{fs};
 
+use std::fs::ReadDir;
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize)]
@@ -7,17 +9,61 @@ struct Workspace {
     folder: String,
 }
 
-fn main() {
-    const WORSPACE_LOCATION: &str="/Users/anttipeltola/Library/Application Support/Code/User/workspaceStorage/a8deed49d145245ed73e0540da56796e/workspace.json";
-    let contents = fs::read_to_string(WORSPACE_LOCATION)
-        .expect("Failed to read file");
-    
-    println!("{}", &contents);
+#[derive(Serialize, Deserialize)]
+struct Alfred_Item {
+    uid: String,
+    title: String
+}
 
-    let w:Workspace= match serde_json::from_str(&contents) {
-        Ok(w) => w,
+fn main() {
+    const WORSPACE_LOCATION: &str="/Users/anttipeltola/Library/Application Support/Code/User/workspaceStorage";
+    // a8deed49d145245ed73e0540da56796e/workspace.json";
+
+    let files: ReadDir= match fs::read_dir(WORSPACE_LOCATION) {
+        Ok(files) => files,
         Err(_) => panic!(),
     };
 
-    println!("{}", &w.folder);
+    let mut worskpaces: Vec<String> = Vec::new();
+    
+    for file in files {
+        let mut file = file.unwrap().path();
+        file.push("workspace.json");
+
+        // println!("{}", &file.display());
+
+        let contents : String= match fs::read_to_string(file) {
+            Ok(contents) => contents,
+            Err(_) => continue,
+        };
+    
+        // println!("{}", &contents);
+
+        let w: Workspace= match serde_json::from_str(&contents) {
+            Ok(w) => w,
+            Err(_) => panic!(),
+        };
+
+        // println!("{}", &w.folder);
+
+        worskpaces.push(w.folder);
+    }
+
+    let mut existing_worskpaces: Vec<String> = Vec::new();
+
+    for space in worskpaces {
+        let space = &space[7..];
+        println!("{}", &space);
+
+        if Path::new(&space).exists() {
+            existing_worskpaces.push(space.to_string());
+        }
+    }
+    
+
+    println!("Existing ones:");
+
+    for space in existing_worskpaces {
+        println!("{}", space);
+    }
 }
